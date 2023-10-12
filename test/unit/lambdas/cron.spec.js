e904b6033e6901171e0bba642580dacb9d44c74a
Martin <martinstojcheski@gmail.com>
2023-10-12 11:18:26 +0200

    fix typo

--- test/unit/lambdas/cron.spec.js
+++ test/unit/lambdas/cron.spec.js
@@ -1,10 +1,10 @@
 const { handler } = require('../../../lambdas/cron');
 
-const { pushRecordsToFifoSQS, querytasksDue } = require('../../../lib/aws');
+const { pushRecordsToFifoSQS, queryTasksDue } = require('../../../lib/aws');
 
 // Mock the dependency function
 jest.mock('../../../lib/aws', () => ({
-    querytasksDue: jest.fn(() => {}),
+    queryTasksDue: jest.fn(() => {}),
     pushRecordsToFifoSQS: jest.fn(() => null),
 }));
 
@@ -15,11 +15,11 @@ describe('Cron Lambda Handler', () => {
 
     it('should query DynamoDB for due tasks and push tasks to SQS queue', async () => {
         const mockData = [{ id: 1 },  {id: 2 }];
-        querytasksDue.mockReturnValue(mockData);
+        queryTasksDue.mockReturnValue(mockData);
         const event = {};
         await handler(event);
         
-        expect(querytasksDue).toHaveBeenCalled();
+        expect(queryTasksDue).toHaveBeenCalled();
         expect(pushRecordsToFifoSQS).toHaveBeenCalledWith(process.env.TASKS_SQS_QUEUE, mockData);
     });
 });
